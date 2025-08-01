ec5ed5a7b1c4b3e1b5d4f1b5683797759cf4cef6
Jongmin Kim <jongmin3725@gmail.com>
2025-08-01 16:02:46 +1000

    login feature made

---
+++ src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react"
+import axios from 'axios';
+import { useNavigate } from "react-router-dom"
+
+export default function Login() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
+
+    const handleLogin = async (e)  => {
+        e.preventDefault();
+        try{
+            const res = await axios.post("http://localhost:8000/api/auth/login/", 
+                {username, password});
+            localStorage.setItem("access", res.data.access);
+            localStorage.setItem("refresh", res.data.refresh);
+            console.log('access', res.data.access);
+            console.log('refresh', res.data.refresh);
+            alert("로그인 성공!");
+            navigate('/')
+        } catch(err) {
+            alert("로그인 실패:" + (err.response?.data?.detail || "오류"));
+        }   
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleLogin}>
+                <h2>로그인</h2>
+                <input
+                type="text"
+                placeholder="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                />
+                <input
+                type="password"
+                placeholder="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
+                <button type="submit">로그인</button>
+            </form>
+        </div>
+    )
+}
